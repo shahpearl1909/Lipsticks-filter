3c9e2626d5e2c4e1e273e60b3c5115627e56a75e
shahpearl1909 <87839222+shahpearl1909@users.noreply.github.com>
2023-01-03 19:03:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+lipstick=loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
+}
+function setup(){
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet has been initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x = "+results[0].pose.nose.x);
+        console.log("nose y = "+results[0].pose.nose.y);
+    }
+}
+function draw(){
+image(video, 0,0,300,300);
+image(lipstick, noseX-15, noseY+10, 40,40);
+
+}
+function picture(){
+    save("lipstick.png");
+}
